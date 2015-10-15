08ad0ec6354007ba34457db3899546460a36bb09
Marco Pariente-Cohen <markypc3@gmail.com>
2015-10-15 18:29:52 -0400

    got started on the data structure. completed the shuffle function and tested its use within a js module. added notes.txt for documentation and wrote out some of the game flow for clarity and ease of implementation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 window.onload = function(){
   console.log("let's get started building exploding kittens!!!");
+  Deck.initialize(4);
 }
+// var types = ['Exploding Kitten']
+
+var explodingKitten = (function () {
+  var name ='';
+  var type = 'explodingKitten';
+})();
+var Deck = (function () {
+  var cards = [1,2,3,4,5,6,7,8,9,0];
+  // var shuffle = ;
+  return {
+    shuffle: function() {
+       for(var j, x, i = cards.length; i; j = Math.floor(Math.random() * i), x = cards[--i], cards[i] = cards[j], cards[j] = x);
+       console.log("Shuffled!");
+    },
+    initialize: function(players){
+      //populate deck to contain the correct amount of cards of each type.
+      //this does not bring the game to a playable state.
+      this.shuffle();
+      console.log(cards.join());
+    }
+
+  }
+})();
